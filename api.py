2e7af00b972d3a8d71b390e89b73551f097a9b44
Siqi Yan <syan235711@gmail.com>
2025-08-30 17:23:06 +0100

    Formatted code

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import threading
 import time
 import uuid
 from sqlite3 import Connection, Cursor
-from typing import Any, Dict, Union
+from typing import Any
 
 import bcrypt
 import flask
@@ -14,21 +14,24 @@ from flask_cors import CORS
 
 # Initiate app with CORS
 app = Flask(__name__)
-CORS(app, origins=['https://army-login.onrender.com'])
+CORS(app, origins=["https://army-login.onrender.com"])
+
 
 # SQLite users connection
 def connect_users() -> tuple[Connection, Cursor]:
-    users_con = sqlite3.connect('db/users.db')
+    users_con = sqlite3.connect("db/users.db")
     users_con.row_factory = sqlite3.Row
     users_cur = users_con.cursor()
     return users_con, users_cur
 
+
 # SQLite tokens connection
 def connect_tokens() -> tuple[Connection, Cursor]:
-    tokens_con = sqlite3.connect('db/tokens.db')
+    tokens_con = sqlite3.connect("db/tokens.db")
     tokens_con.row_factory = sqlite3.Row
     tokens_cur = tokens_con.cursor()
     return tokens_con, tokens_cur
+
 
 # SQL queries
 def run_query(db: str, query: str, params: tuple = ()) -> list[Any]:
@@ -39,32 +42,61 @@ def run_query(db: str, query: str, params: tuple = ()) -> list[Any]:
         con.commit()
         return cur.fetchall()
 
-admin_passw = b'$2b$12$63yU9pT5PqmE.kfVbhiwVuDavrEB1g.9X17uY5N4tyKSJtm3cf66W' # hello
+
+admin_passw = b"$2b$12$63yU9pT5PqmE.kfVbhiwVuDavrEB1g.9X17uY5N4tyKSJtm3cf66W"  # hello
+
 
 # Create Token
 def create_token(role: str) -> str:
     token_con, _ = connect_tokens()
     token = str(uuid.uuid4())
     with token_con as con:
-        con.execute('''
+        con.execute(
+            """
         INSERT INTO tokens (token, expiry, role)
         VALUES
             (?, ?, ?)
-        ''', (token, str(datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=15)), role))
+        """,
+            (
+                token,
+                str(
+                    datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=15)
+                ),
+                role,
+            ),
+        )
         con.commit()
     return token
 
+
 # User exists function
 def user_exists(username: str) -> bool:
-    res = run_query('db/users.db', 'SELECT * FROM users WHERE name = ?', (username,))
-    return not(res == list())
+    res: list[Any] = run_query(
+        "db/users.db", "SELECT * FROM users WHERE name = ?", (username,)
+    )
+    return res != list()
+
+
+# Token exists function
+def token_exists(token: str) -> bool:
+    res = run_query("db/token.db", "SELECT * FROM tokens WHERE token = ?", (token,))
+    return res != list()
+
 
 # Return True if any of the arguements are '' or None
 def AnyEmpty(*args: str) -> bool:
-    return any(arg in ('', None) for arg in args)
+    return any(arg in ("", None) for arg in args)
+
+
+# isAdmin function
+# Assumes token exists
+def isAdmin(token: str) -> bool:
+    res = run_query("db/token.db", "SELECT * FROM tokens WHERE token = ?", (token,))
+    return res[0]["role"] == "admin"
+
 
 # Ping function
-def ping():
+def ping() -> None:
     while True:
         try:
             requests.get("https://army-api.onrender.com/health", timeout=10)
@@ -73,142 +105,192 @@ def ping():
             pass
         time.sleep(600)
 
+
 threading.Thread(target=ping, daemon=True).start()
 
+
 # Health Endpoint
-@app.route('/health', methods=['POST', 'GET'])
-def health():
-    return jsonify({'online': True})
+@app.route("/health", methods=["POST", "GET"])
+def health() -> flask.Response:
+    return jsonify({"online": True})
+
 
 # Login route
-@app.route('/login', methods = ['POST'])
-def login() -> Union[flask.Response, tuple[flask.Response, int]]:
+@app.route("/login", methods=["POST"])
+def login() -> flask.Response | tuple[flask.Response, int]:
     # Test requests body type
-    json: Dict[str, str] = request.get_json()
+    json: dict[str, str] = request.get_json()
     if json is None:
-        return jsonify({'IsSuccess': False, 'Error': 'No JSON data sent'})
+        return jsonify({"IsSuccess": False, "Error": "No JSON data sent"})
     # Get requests JSON
-    username: str = json.get('user', '')
-    passw: str = json.get('password', '')
+    username: str = json.get("user", "")
+    passw: str = json.get("password", "")
     if AnyEmpty(username, passw):
-        return jsonify({'IsSuccess': False, 'Error': 'User or Password field missing'})
+        return jsonify({"IsSuccess": False, "Error": "User or Password field missing"})
     # Logic
     if not (username and user_exists(username)):
-        return jsonify({'IsSuccess': False, 'Error': "User doesn't exist"}), 404
+        return jsonify({"IsSuccess": False, "Error": "User doesn't exist"}), 404
     # Get user from db
-    user = dict(run_query('db/users.db', 'SELECT * FROM users WHERE name = ?', (username,))[0])
-    if not bcrypt.checkpw(passw.encode('utf-8'), user['hashpw'].encode()):
-        return jsonify({'IsSuccess': False, 'Error': "Unauthorised"}), 403
+    user = run_query("db/users.db", "SELECT * FROM users WHERE name = ?", (username,))[
+        0
+    ]
+    if not bcrypt.checkpw(passw.encode("utf-8"), user["hashpw"].encode()):
+        return jsonify({"IsSuccess": False, "Error": "Unauthorised"}), 403
     else:
-        if user['role'] == 'admin':
-            return jsonify({'IsSuccess': True, 'Message': f'User {username} authorised, type admin', 'Token': create_token('admin')})
+        if user["role"] == "admin":
+            return jsonify(
+                {
+                    "IsSuccess": True,
+                    "Message": f"User {username} authorised, type admin",
+                    "Token": create_token("admin"),
+                }
+            )
         else:
-            return jsonify({'IsSuccess': True, 'Message': f'User {username} authorised, type normal', 'Token': create_token('user')})
+            return jsonify(
+                {
+                    "IsSuccess": True,
+                    "Message": f"User {username} authorised, type normal",
+                    "Token": create_token("user"),
+                }
+            )
 
-@app.route('/register', methods = ['POST'])
-def register() -> Union[flask.Response, tuple[flask.Response, int]]:
+
+@app.route("/register", methods=["POST"])
+def register() -> flask.Response | tuple[flask.Response, int]:
     # Test requests body type
-    json: Dict[str, str] = request.get_json()
+    json: dict[str, str] = request.get_json()
     if json is None:
-        return jsonify({'IsSuccess': False, 'Error': 'No JSON data sent'})
+        return jsonify({"IsSuccess": False, "Error": "No JSON data sent"})
     # Get requests JSON
-    username: str = json.get('user', '')
-    passw: str = json.get('password', '')
-    user_class: str = json.get('class', '')
-    register_passw: str = json.get('reg_password', '')
+    username: str = json.get("user", "")
+    passw: str = json.get("password", "")
+    user_class: str = json.get("class", "")
+    register_passw: str = json.get("reg_password", "")
     if AnyEmpty(username, passw, user_class, register_passw):
-        return jsonify({'IsSuccess': False, 'Error': 'One or more field(s) missing'})
+        return jsonify({"IsSuccess": False, "Error": "One or more field(s) missing"})
     # Logic
     if not bcrypt.checkpw(register_passw.encode(), admin_passw):
-        return jsonify({'IsSuccess': False, 'Error': 'Unauthorised'}), 403
+        return jsonify({"IsSuccess": False, "Error": "Unauthorised"}), 403
     elif user_exists(username):
-        return jsonify({'IsSuccess': False, 'Error': 'User already exists'}), 400
+        return jsonify({"IsSuccess": False, "Error": "User already exists"}), 400
     else:
         # Register if the logic was succesful
         users_con, _ = connect_users()
         with users_con as con:
-            con.execute('''
+            con.execute(
+                """
             INSERT INTO users(name, hashpw, role)
             VALUES
                 (?, ?, ?)
-            ''', (username, bcrypt.hashpw(passw.encode(), bcrypt.gensalt()).decode(), user_class))
+            """,
+                (
+                    username,
+                    bcrypt.hashpw(passw.encode(), bcrypt.gensalt()).decode(),
+                    user_class,
+                ),
+            )
             con.commit()
-        return jsonify({'IsSuccess': True, 'Message': f'User {username}, class {user_class}, registered'})
+        return jsonify(
+            {
+                "IsSuccess": True,
+                "Message": f"User {username}, class {user_class}, registered",
+            }
+        )
 
-@app.route('/check-token', methods=['POST'])
-def check_token() -> Union[flask.Response, tuple[flask.Response, int]]:
+
+@app.route("/check-token", methods=["POST"])
+def check_token() -> flask.Response | tuple[flask.Response, int]:
     # Test requests body type
-    json: Dict[str, str] = request.get_json()
+    json: dict[str, str] = request.get_json()
     if json is None:
-        return jsonify({'IsSuccess': False, 'Error': 'No JSON data sent'})
+        return jsonify({"IsSuccess": False, "Error": "No JSON data sent"})
     # Get requests JSON
-    token: str = json.get('token', '')
+    token: str = json.get("token", "")
     if AnyEmpty(token):
-        return jsonify({'IsSuccess': False, 'Error': 'Token Field missing'})
+        return jsonify({"IsSuccess": False, "Error": "Token Field missing"})
     # Check if token exists
-    id = run_query('db/tokens.db', 'SELECT id FROM tokens WHERE token = ?', (token,))
-    if id != list():
-        id = dict(id[0])['id']
+    if token_exists(token):
         token_con, _ = connect_tokens()
         with token_con as con:
-            res = con.execute('SELECT * FROM tokens WHERE id = ?', (id,))
+            res = con.execute("SELECT * FROM tokens WHERE token = ?", (token,))
             tokenSQL = res.fetchone()
-            if datetime.datetime.fromisoformat(tokenSQL['expiry']) > datetime.datetime.now(datetime.UTC):
-                con.execute('UPDATE tokens SET expiry = ? WHERE id = ?', 
-                              ((datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=15)).isoformat(), 
-                                id))
+            if datetime.datetime.fromisoformat(
+                tokenSQL["expiry"]
+            ) > datetime.datetime.now(datetime.UTC):
+                con.execute(
+                    "UPDATE tokens SET expiry = ? WHERE token = ?",
+                    (
+                        (
+                            datetime.datetime.now(datetime.UTC)
+                            + datetime.timedelta(minutes=15)
+                        ).isoformat(),
+                        token,
+                    ),
+                )
                 con.commit()
-                return jsonify({'IsSuccess': True, 'Role': tokenSQL['role']})
+                return jsonify({"IsSuccess": True, "Role": tokenSQL["role"]})
             else:
-                con.execute('DELETE FROM tokens WHERE id = ?', (id,))
+                con.execute("DELETE FROM tokens WHERE token = ?", (token,))
                 con.commit()
-                return jsonify({'IsSuccess': False, 'Error': f'Token {tokenSQL['token']} expired'})
-    return jsonify({'IsSuccess': False, 'Error': 'Token Not Recognized'}), 403
+                return jsonify({"IsSuccess": False, "Error": f"Token {token} expired"})
+    return jsonify({"IsSuccess": False, "Error": "Token Not Recognized"}), 403
+
 
 # Delete Token
-@app.route('/delete-token', methods=['POST'])
-def delete_token() -> Union[flask.Response, tuple[flask.Response, int]]:
+@app.route("/delete-token", methods=["POST"])
+def delete_token() -> flask.Response | tuple[flask.Response, int]:
     # Test requests body type
-    json: Dict[str, str] = request.get_json()
+    json: dict[str, str] = request.get_json()
     if json is None:
-        return jsonify({'IsSuccess': False, 'Error': 'No JSON data sent'})
+        return jsonify({"IsSuccess": False, "Error": "No JSON data sent"})
     # Get requests JSON
-    token: str = json.get('token', '')
+    token: str = json.get("token", "")
     if AnyEmpty(token):
-        return jsonify({'IsSuccess': False, 'Error': 'Token field missing'})
+        return jsonify({"IsSuccess": False, "Error": "Token field missing"})
     # Confirm token exists
-    check_token: list[Any] = run_query('db/tokens.db', 'SELECT * FROM tokens WHERE token = ?', (token,))
+    check_token: list[Any] = run_query(
+        "db/tokens.db", "SELECT * FROM tokens WHERE token = ?", (token,)
+    )
     if check_token == []:
-        return jsonify({'IsSuccess': False, 'Error': 'Token not found'})
+        return jsonify({"IsSuccess": False, "Error": "Token not found"})
     # Delete token
     token_con, _ = connect_tokens()
     with token_con as con:
-        con.execute('''
+        con.execute(
+            """
         DELETE FROM tokens WHERE token = ?
-        ''', (token,))
+        """,
+            (token,),
+        )
         con.commit()
-        return jsonify({'IsSuccess': True, 'Message': f'Token {token} deleted'})
+        return jsonify({"IsSuccess": True, "Message": f"Token {token} deleted"})
+
 
 # Delete user
-@app.route('/delete-user', methods=['POST'])
-def delete_user() -> Union[flask.Response, tuple[flask.Response, int]]:
+@app.route("/delete-user", methods=["POST"])
+def delete_user() -> flask.Response | tuple[flask.Response, int]:
     # Test requests body type
-    json: Dict[str, str] = request.get_json()
+    json: dict[str, str] = request.get_json()
     if json is None:
-        return jsonify({'IsSuccess': False, 'Error': 'No JSON data sent'})
+        return jsonify({"IsSuccess": False, "Error": "No JSON data sent"})
     # Get requests JSON
-    user: str = json.get('user', '')
+    user: str = json.get("user", "")
     if AnyEmpty(user):
-        return jsonify({'IsSuccess': False, 'Error': 'User field missing'})
+        return jsonify({"IsSuccess": False, "Error": "User field missing"})
     # Confirm user exists
-    check_user: list[Any] = run_query('db/users.db', 'SELECT * FROM users WHERE name = ?', (user,))
+    check_user: list[Any] = run_query(
+        "db/users.db", "SELECT * FROM users WHERE name = ?", (user,)
+    )
     if check_user == []:
-        return jsonify({'IsSuccess': False, 'Error': 'User not found'})
+        return jsonify({"IsSuccess": False, "Error": "User not found"})
     # Delete user
     user_con, _ = connect_users()
     with user_con as con:
-        con.execute('''
+        con.execute(
+            """
         DELETE FROM users WHERE name = ?
-        ''', (user,))
+        """,
+            (user,),
+        )
         con.commit()
-        return jsonify({'IsSuccess': True, 'Message': f'User {user} deleted'})
+        return jsonify({"IsSuccess": True, "Message": f"User {user} deleted"})
